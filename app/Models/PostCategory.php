535276040394e607e1b1a942637ebae2247ee20e
As3ad Altabali <as3ad.moh@gmail.com>
2025-01-28 15:52:39 +0300

    feat: add PostCategory model and migration

---
+++ app/Models/PostCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostCategory extends Model
+{
+    protected $fillable = ['name', 'slug', 'parent_id', 'order_column', 'is_active'];
+
+    public function parent()
+    {
+        return $this->belongsTo(PostCategory::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(PostCategory::class, 'parent_id');
+    }
+}
