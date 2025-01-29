5765eee4533a722105fa717cc36137ece97ae9ab
Mohammed Farea <qubatim95@gmail.com>
2025-01-29 14:01:45 +0300

    chore: change branch icon

--- app/Filament/Resources/BrancheResource.php
+++ app/Filament/Resources/BrancheResource.php
@@ -17,7 +17,7 @@ class BrancheResource extends Resource
 {
     protected static ?string $model = Branche::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
 
     public static function form(Form $form): Form
     {
