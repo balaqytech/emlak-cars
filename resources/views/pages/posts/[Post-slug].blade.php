944a9807739a43a0b62880c6bbb64c13b463169c
As3ad Altabali <as3ad.moh@gmail.com>
2025-02-07 16:30:29 +0300

    feat: add post single layout and add Tailwind typography plugin

---
+++ resources/views/pages/posts/[Post-slug].blade.php
@@ -0,0 +1,35 @@
+@php
+    use function Laravel\Folio\name;
+
+    name('posts.show');
+@endphp
+
+<x-app-layout>
+    <x-slot name="title">
+        {{ $post->title }}
+    </x-slot>
+
+    <div class="w-full h-[80dvh] overflow-hidden bg-center bg-cover">
+        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->title }}"
+            class="w-full h-full object-cover object-center">
+    </div>
+
+    <article class="max-w-4xl mx-auto p-6">
+        <header
+            class="flex flex-col items-center justify-center gap-4 mb-24 -mt-36 relative bg-white p-20 text-center shadow-3xl rounded-lg">
+            <div>
+                <a href="#" class="bg-primary text-white rounded-sm px-5 py-2">{{ $post->category->name }}</a>
+            </div>
+            <h1 class="text-5xl font-bold text-slate-800">{{ $post->title }}</h1>
+            <div class="text-sm mt-2">
+                <p>{{ __('frontend.published_at') }} <time
+                        datetime="{{ $post->published_at }}">{{ $post->published_at->format('d/m/Y') }}</time></p>
+            </div>
+        </header>
+
+        <section class="prose max-w-none">
+            {!! $post->content !!}
+        </section>
+    </article>
+
+</x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,24 @@ export default {
             },
             colors: {
                 'primary': '#af1f23',
-            }
+            },
+            boxShadow: {
+                '3xl': '0 0 30px rgba(0,0,0,.08)',
+                '4xl': '0 0 45px rgba(0,0,0,.09)',
+            },
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        color: theme('colors.slate.600'),
+                        a: {
+                            color: theme('colors.primary'),
+                        },
+                    },
+                },
+            }),
         },
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 };
