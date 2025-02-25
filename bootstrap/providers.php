bede61d9176bf5bfbf462fb4e04c19ded7542e2c
As3ad Altabali <as3ad.moh@gmail.com>
2025-02-25 08:26:13 +0300

    feat: integrate OwenIt Laravel Auditing package for model change tracking

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FolioServiceProvider::class,
+    OwenIt\Auditing\AuditingServiceProvider::class,
 ];
