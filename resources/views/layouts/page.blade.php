53e48c7e04055191e45d336538ea54c8c8e4ed75
As3ad Altabali <as3ad.moh@gmail.com>
2025-02-26 05:34:34 +0300

    feat: add page title pattern SVG and update layout styling for enhanced visual appeal

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -3,7 +3,8 @@
         {{ $title }}
     </x-slot>
 
-    <section class="relative min-h-[60vh] flex items-center justify-center bg-primary text-white bg-cover bg-center after:content-[''] after:absolute after:inset-0 after:bg-slate-950 after:opacity-15" style="background-image: url({{ asset('storage/' . general_settings('site_banner')) }});">
+    <section class="relative min-h-[60vh] flex items-center justify-center bg-primary text-white bg-cover bg-center after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-r after:from-slate-950 after:to-transparent after:opacity-55 before:content-[''] before:absolute before:inset-0 before:bg-cover before:bg-center  before:bg-[url('{{ env('APP_URL') }}/images/page-title-pattern.svg')] before:opacity-15" style="background-image: url({{ asset('storage/' . general_settings('site_banner')) }});">
+
         <div class="wrapper relative z-10">
             <h1 class="text-5xl font-bold text-center">{{ $title }}</h1>
             <p class="text-center mt-4">{{ $excerpt ?? '' }}</p>
