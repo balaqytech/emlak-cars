234c6def24c94e0b96c713e31394ca297703cbe5
As3ad Altabali <as3ad.moh@gmail.com>
2025-03-04 09:57:56 +0300

    style: fix vehicle details page layout

--- resources/views/pages/vehicles/[Vehicle-slug]/index.blade.php
+++ resources/views/pages/vehicles/[Vehicle-slug]/index.blade.php
@@ -30,51 +30,58 @@
                 </p>
             </div>
             <div class="h-48 w-full">
-                <img class="w-full h-full object-contain object-center" src="{{ asset('storage/' . $vehicle->brand->logo) }}" alt="{{ $vehicle->brand->name }}">
+                <img class="w-full h-full object-contain object-center"
+                    src="{{ asset('storage/' . $vehicle->brand->logo) }}" alt="{{ $vehicle->brand->name }}">
             </div>
 
         </div>
     </section>
 
     <section>
-        <div class="wrapper">
-            <nav class="relative z-0 flex flex-col md:flex-row border rounded-xl overflow-hidden" aria-label="Tabs" role="tablist"
-                aria-orientation="horizontal">
-                <button type="button"
-                    class="hs-tab-active:border-b-primary hs-tab-active:text-gray-900 relative min-w-0 flex-1 bg-white first:border-s-0 border-s border-b-2 py-4 px-4 text-gray-500 hover:text-gray-700 text-sm font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none active"
-                    id="models-item" aria-selected="true" data-hs-tab="#models"
-                    aria-controls="models" role="tab">
+        <div x-data="{ selectedTab: 'models' }" class="wrapper">
+            <nav x-on:keydown.right.prevent="$focus.wrap().next()" x-on:keydown.left.prevent="$focus.wrap().previous()"
+                class="relative z-0 flex flex-col md:flex-row border rounded-xl overflow-hidden" role="tablist"
+                aria-label="tab options">
+                <button type="button" x-on:click="selectedTab = 'models'"
+                    x-bind:aria-selected="selectedTab === 'models'"
+                    x-bind:tabindex="selectedTab === 'models' ? '0' : '-1'"
+                    x-bind:class="selectedTab === 'models' ? 'border-b-primary text-gray-900' : 'text-gray-500'"
+                    class="relative min-w-0 flex-1 bg-white first:border-s-0 border-s border-b-2 py-4 px-4 hover:text-gray-700 text-sm font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none active"
+                    id="models-item" aria-selected="true" data-hs-tab="#models" aria-controls="models" role="tab">
                     {{ __('frontend.vehicles.models') }}
                 </button>
-                <button type="button"
-                    class="hs-tab-active:border-b-primary hs-tab-active:text-gray-900 relative min-w-0 flex-1 bg-white first:border-s-0 border-s border-b-2 py-4 px-4 text-gray-500 hover:text-gray-700 text-sm font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none"
-                    id="overview-item" aria-selected="false" data-hs-tab="#overview"
-                    aria-controls="overview" role="tab">
+                <button type="button" x-on:click="selectedTab = 'overview'"
+                    x-bind:aria-selected="selectedTab === 'overview'"
+                    x-bind:tabindex="selectedTab === 'overview' ? '0' : '-1'"
+                    x-bind:class="selectedTab === 'overview' ? 'border-b-primary text-gray-900' : 'text-gray-500'"
+                    class="relative min-w-0 flex-1 bg-white first:border-s-0 border-s border-b-2 py-4 px-4 hover:text-gray-700 text-sm font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none active"
+                    id="overview-item" aria-selected="true" data-hs-tab="#overview" aria-controls="overview" role="tab">
                     {{ __('frontend.vehicles.overview') }}
                 </button>
-                <button type="button"
-                    class="hs-tab-active:border-b-primary hs-tab-active:text-gray-900 relative min-w-0 flex-1 bg-white first:border-s-0 border-s border-b-2 py-4 px-4 text-gray-500 hover:text-gray-700 text-sm font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none"
-                    id="features-item" aria-selected="false" data-hs-tab="#features"
-                    aria-controls="features" role="tab">
+                <button type="button" x-on:click="selectedTab = 'features'"
+                    x-bind:aria-selected="selectedTab === 'features'"
+                    x-bind:tabindex="selectedTab === 'features' ? '0' : '-1'"
+                    x-bind:class="selectedTab === 'features' ? 'border-b-primary text-gray-900' : 'text-gray-500'"
+                    class="relative min-w-0 flex-1 bg-white first:border-s-0 border-s border-b-2 py-4 px-4 hover:text-gray-700 text-sm font-medium text-center overflow-hidden hover:bg-gray-50 focus:z-10 focus:outline-none focus:text-primary disabled:opacity-50 disabled:pointer-events-none active"
+                    id="features-item" aria-selected="true" data-hs-tab="#features" aria-controls="features" role="tab">
                     {{ __('frontend.vehicles.features') }}
                 </button>
             </nav>
 
             <div class="mt-3">
-                <div id="models" class="py-8" role="tabpanel" aria-labelledby="models-item">
+                <div x-cloak x-show="selectedTab === 'models'" id="tabpanelModels" role="tabpanel" aria-label="models" class="py-8" role="tabpanel" aria-labelledby="models-item">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                         @foreach ($vehicle->vehicleModels as $model)
                             <x-vehicle-model-card :model="$model" />
                         @endforeach
                     </div>
                 </div>
-                <div id="overview" class="hidden" role="tabpanel" aria-labelledby="overview-item">
+                <div x-cloak x-show="selectedTab === 'overview'" id="tabpanelOverview" role="tabpanel" aria-label="overview">
                     <div class="prose max-w-3xl py-12 mx-auto">
                         {!! str($vehicle->overview)->sanitizeHtml() !!}
                     </div>
                 </div>
-                <div id="features" class="hidden" role="tabpanel"
-                    aria-labelledby="features-item">
+                <div x-cloak x-show="selectedTab === 'features'" id="tabpanelFeatures" role="tabpanel" aria-label="features">
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         @foreach ($vehicle->features as $feature)
                             <x-vehicle-feature-card :image="$feature['image']" :title="$feature['title']" :description="$feature['description']" />
