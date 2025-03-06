742911e6fb0728233fcbd4db26543c934c4392a4
As3ad Altabali <as3ad.moh@gmail.com>
2025-03-06 08:39:19 +0300

    feat: enhance header navigation with mobile menu support and improved layout

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,2 +1,4 @@
-<a class="{{ request()->is(ltrim($link, '/')) ? 'text-primary underline font-bold' : 'text-slate-600' }} hover:text-slate-400 focus:outline-none focus:text-slate-400"
-    href="{{ $link }}">{{ $title }}</a>
+<li class="py-4">
+    <a class="{{ request()->is(ltrim($link, '/')) ? 'text-primary underline font-bold' : 'text-slate-600' }} hover:text-slate-400 focus:outline-none focus:text-slate-400"
+        href="{{ $link }}">{{ $title }}</a>
+</li>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,44 +1,48 @@
-<header class="sticky flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 border-b border-gray-200 z-50 shadow-3xl">
-    <nav class="wrapper px-4 sm:flex sm:items-center sm:justify-between">
-        <div class="flex items-center justify-between">
-            <a class="flex-none text-xl font-semibold focus:outline-none focus:opacity-80" href="/"
-                aria-label="Brand">
-                <img class="h-16" src="{{ asset('storage/' . general_settings('site_logo')) }}" alt="{{ general_settings('site_name') }}">
-            </a>
-            <div class="sm:hidden">
-                <button type="button"
-                    class="hs-collapse-toggle relative size-7 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-                    id="hs-navbar-example-collapse" aria-expanded="false" aria-controls="hs-navbar-example"
-                    aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-example">
-                    <svg class="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="3" x2="21" y1="6" y2="6" />
-                        <line x1="3" x2="21" y1="12" y2="12" />
-                        <line x1="3" x2="21" y1="18" y2="18" />
-                    </svg>
-                    <svg class="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
-                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M18 6 6 18" />
-                        <path d="m6 6 12 12" />
-                    </svg>
-                    <span class="sr-only">Toggle navigation</span>
-                </button>
-            </div>
-        </div>
-        <div id="hs-navbar-example"
-            class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
-            aria-labelledby="hs-navbar-example-collapse">
-            <nav class="flex flex-col gap-5 text-base mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-                <x-nav-link link="/" title="{{ __('frontend.navigation.home') }}" />
-                <x-nav-link link="/vehicles" title="{{ __('frontend.navigation.vehicles') }}" />
-                <x-nav-link link="/offers" title="{{ __('frontend.navigation.offers') }}" />
-                <x-nav-link link="/posts" title="{{ __('frontend.navigation.posts') }}" />
-                <x-nav-link link="/branches" title="{{ __('frontend.navigation.branches') }}" />
-                <x-nav-link link="/about" title="{{ __('frontend.navigation.about') }}" />
-                <x-nav-link link="/contact" title="{{ __('frontend.navigation.contact') }}" />
-            </nav>
-        </div>
+<header x-data="{ mobileMenuIsOpen: false }" x-on:click.away="mobileMenuIsOpen = false"
+    class=" w-full bg-white text-sm py-3 border-b border-gray-200 z-50 shadow-3xl">
+    <nav class="wrapper flex items-center justify-between gap-4">
+        <a class="flex-none text-xl font-semibold focus:outline-none focus:opacity-80" href="/"
+            aria-label="Brand">
+            <img class="h-16" src="{{ asset('storage/' . general_settings('site_logo')) }}"
+                alt="{{ general_settings('site_name') }}">
+        </a>
+        <ul class="hidden items-center md:flex gap-8 justify-center">
+            <x-nav-link link="/" title="{{ __('frontend.navigation.home') }}" />
+            <x-nav-link link="/vehicles" title="{{ __('frontend.navigation.vehicles') }}" />
+            <x-nav-link link="/offers" title="{{ __('frontend.navigation.offers') }}" />
+            <x-nav-link link="/posts" title="{{ __('frontend.navigation.posts') }}" />
+            <x-nav-link link="/branches" title="{{ __('frontend.navigation.branches') }}" />
+            <x-nav-link link="/about" title="{{ __('frontend.navigation.about') }}" />
+        </ul>
+        <x-primary-button href="/contact" class="hidden md:block">{{ __('frontend.navigation.contact') }}</x-primary-button>
+        <button x-on:click="mobileMenuIsOpen = !mobileMenuIsOpen" x-bind:aria-expanded="mobileMenuIsOpen"
+            x-bind:class="mobileMenuIsOpen ? 'fixed top-6 right-6 z-20' : null" type="button"
+            class="flex text-on-surface dark:text-on-surface-dark md:hidden" aria-label="mobile menu"
+            aria-controls="mobileMenu">
+            <svg x-cloak x-show="!mobileMenuIsOpen" xmlns="http://www.w3.org/2000/svg" fill="none"
+                aria-hidden="true" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            <svg x-cloak x-show="mobileMenuIsOpen" xmlns="http://www.w3.org/2000/svg" fill="none"
+                aria-hidden="true" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+        </button>
+        {{-- mobile menu --}}
+        <ul x-cloak x-show="mobileMenuIsOpen"
+            x-transition:enter="transition motion-reduce:transition-none ease-out duration-300"
+            x-transition:enter-start="-translate-y-full" x-transition:enter-end="translate-y-0"
+            x-transition:leave="transition motion-reduce:transition-none ease-out duration-300"
+            x-transition:leave-start="translate-y-0" x-transition:leave-end="-translate-y-full" id="mobileMenu"
+            class="fixed max-h-svh overflow-y-auto inset-x-0 top-0 z-10 flex flex-col bg-white divide-y divide-outline rounded-b-radius border-b border-outline bg-surface-alt px-6 pb-6 pt-20">
+            <x-nav-link link="/" title="{{ __('frontend.navigation.home') }}" />
+            <x-nav-link link="/vehicles" title="{{ __('frontend.navigation.vehicles') }}" />
+            <x-nav-link link="/offers" title="{{ __('frontend.navigation.offers') }}" />
+            <x-nav-link link="/posts" title="{{ __('frontend.navigation.posts') }}" />
+            <x-nav-link link="/branches" title="{{ __('frontend.navigation.branches') }}" />
+            <x-nav-link link="/about" title="{{ __('frontend.navigation.about') }}" />
+            <x-nav-link link="/contact" title="{{ __('frontend.navigation.contact') }}" />
+        </ul>
     </nav>
 </header>
