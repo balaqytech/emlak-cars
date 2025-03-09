8737662a3bd32f2fc5c8851616cf1169a7d2dd8f
As3ad Altabali <as3ad.moh@gmail.com>
2025-03-09 10:16:44 +0300

    style: update routing to redirect root to Arabic locale

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Livewire\InstallmentCalculator;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/installment-calculator', InstallmentCalculator::class)->name('installment-calculator');
+Route::redirect('/', '/ar');
