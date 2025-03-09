a02ece9872d05a072b4d208b15a6b1d87e79c26f
As3ad Altabali <as3ad.moh@gmail.com>
2025-03-09 19:03:11 +0300

    feat: add fallback locale for translation to Arabic in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Spatie\Translatable\Facades\Translatable::fallback(
+            fallbackLocale: 'ar',
+        );
     }
 }
