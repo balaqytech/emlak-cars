7c949e482730f202297e1972fde3576aa8e52983
As3ad Altabali <as3ad.moh@gmail.com>
2025-04-30 08:54:59 +0300

    style: improve layout and spacing for brand and category filters in featured vehicles

--- resources/views/livewire/featured-vehicles.blade.php
+++ resources/views/livewire/featured-vehicles.blade.php
@@ -1,21 +1,31 @@
 <div class="h-auto w-full">
-    <div class="flex items-center justify-center gap-8 overflow-x-auto mb-6">
+    <div class="flex justify-center gap-4 overflow-x-auto mb-6 py-2">
+        <div class="flex items-center cursor-pointer px-2 shrink-0 h-auto rounded-lg shadow border-2 @if ($selectedBrand == null) border-primary @else border-slate-500 @endif hover:-translate-y-1 transition-all duration-300 ease-in-out"
+            wire:click="filterByBrand('')">
+            {{ __('frontend.all') }}
+        </div>
         @foreach ($brands as $brand)
-            <div class="flex items-center shrink-0 h-32 w-32 p-1 cursor-pointer rounded-lg shadow-lg border-2 @if($selectedBrand == $brand->id) border-primary @else border-slate-500 @endif" wire:click="filterByBrand({{ $brand->id }})" wire:key="brand-{{ $brand->id }}">
-                <img src="{{ asset('/storage/' . $brand->logo) }}" alt="{{ $brand->name }}"
-                    class="w-full h-full object-cover object-center rounded-lg">
+            <div class="flex items-center gap-2 shrink-0 p-1 cursor-pointer rounded-lg shadow border-2 @if ($selectedBrand == $brand->id) border-primary @else border-slate-500 @endif hover:-translate-y-1 transition-all duration-300 ease-in-out"
+                wire:click="filterByBrand({{ $brand->id }})" wire:key="brand-{{ $brand->id }}">
+                <div class="h-12 w-12">
+                    <img src="{{ asset('/storage/' . $brand->logo) }}" alt="{{ $brand->name }}"
+                        class="w-full h-full object-cover object-center rounded-lg">
+                </div>
+                <span class="pe-2">{{ $brand->name }}</span>
             </div>
         @endforeach
     </div>
-    <div class="flex flex-nowrap items-center justify-start gap-8 overflow-x-auto mb-6">
-        <div class="cursor-pointer shrink-0 @if($selectedCategory == null) border-b-2 border-slate-500 @endif" wire:click="filterByCategory('')">
+    <div class="flex flex-nowrap items-center justify-start gap-8 overflow-x-auto mb-6 py-1">
+        <div class="cursor-pointer shrink-0 @if ($selectedCategory == null) border-b-2 border-slate-500 @endif"
+            wire:click="filterByCategory('')">
             {{ __('frontend.all') }}
-        </div>            
+        </div>
         @foreach ($categories as $category)
-            <div class="cursor-pointer shrink-0 @if($selectedCategory == $category->id)border-b-2 border-slate-500 @endif" wire:click="filterByCategory({{ $category->id }})" wire:key="category-{{ $category->id }}">
+            <div class="cursor-pointer shrink-0 @if ($selectedCategory == $category->id) border-b-2 border-slate-500 @endif"
+                wire:click="filterByCategory({{ $category->id }})" wire:key="category-{{ $category->id }}">
                 {{ $category->name }}
-            </div>            
-        @endforeach    
+            </div>
+        @endforeach
     </div>
     {{-- <div class="swiper h-auto w-full"
         data-swiper-options="{
@@ -50,10 +60,10 @@
                 <x-vehicle-card :vehicle="$vehicle" />
             </div>
         @empty
-        <div class="mt-8 col-span-3">
-            <x-icons.question class="block mx-auto size-24 text-primary" />
-            <p class="text-center">{{ __('frontend.vehicles.no_vehicles') }}</p>
-        </div>
+            <div class="mt-8 col-span-3">
+                <x-icons.question class="block mx-auto size-24 text-primary" />
+                <p class="text-center">{{ __('frontend.vehicles.no_vehicles') }}</p>
+            </div>
         @endforelse
     </div>
 </div>
