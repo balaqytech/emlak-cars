55140b786cfc3f659e13a5697f9ccec587f6aacc
As3ad Altabali <as3ad.moh@gmail.com>
2025-06-25 06:43:51 +0300

    feat: add VehicleModelPolicy for authorization management of vehicle models

---
+++ app/Policies/VehicleModelPolicy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\VehicleModel;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VehicleModelPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_vehicle::model');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, VehicleModel $vehicleModel): bool
+    {
+        return $user->can('view_vehicle::model');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_vehicle::model');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, VehicleModel $vehicleModel): bool
+    {
+        return $user->can('update_vehicle::model');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, VehicleModel $vehicleModel): bool
+    {
+        return $user->can('delete_vehicle::model');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('{{ DeleteAny }}');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     */
+    public function forceDelete(User $user, VehicleModel $vehicleModel): bool
+    {
+        return $user->can('{{ ForceDelete }}');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('{{ ForceDeleteAny }}');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, VehicleModel $vehicleModel): bool
+    {
+        return $user->can('{{ Restore }}');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('{{ RestoreAny }}');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, VehicleModel $vehicleModel): bool
+    {
+        return $user->can('{{ Replicate }}');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('{{ Reorder }}');
+    }
+}
