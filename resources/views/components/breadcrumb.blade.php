5520cad57b841d981d41556a991ed653f9f1b866
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-10 04:41:20 +0300

    feat: enhance breadcrumb component with JSON-LD structured data and improve accessibility

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,12 +1,38 @@
 @props(['items' => [], 'color' => 'slate-400'])
 
+{{-- Add JSON-LD for Breadcrumbs --}}
+<script type="application/ld+json">
+{
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+        {
+            "@type": "ListItem",
+            "position": 1,
+            "name": "{{ __('frontend.home') }}",
+            "item": "{{ localizedUrl('/') }}"
+        }
+        @foreach ($items as $index => $item)
+        ,{
+            "@type": "ListItem",
+            "position": {{ $loop->iteration + 1 }},
+            "name": "{{ $item['label'] }}",
+            "item": "{{ $item['url'] ?? '#' }}"
+        }
+        @endforeach
+    ]
+}
+</script>
+
+{{-- Render the Breadcrumb HTML --}}
 <ol {{ $attributes }}>
     <li class="inline-flex items-center">
         <a class="flex items-center text-xs text-{{ $color }} hover:text-primary focus:outline-hidden focus:text-primary"
-            href="{{ localizedUrl('/') }}">
+            href="{{ localizedUrl('/') }}"
+            aria-label="{{ __('frontend.home') }}">
             {{ __('frontend.home') }}
         </a>
-        @if(count($items) > 0)
+        @if (count($items) > 0)
             <x-icons.slash class="shrink-0 size-5 text-gray-400 mx-2" />
         @endif
     </li>
@@ -15,7 +41,8 @@
         <li class="inline-flex items-center">
             @if (!$loop->last)
                 <a class="flex items-center text-xs text-{{ $color }} hover:text-primary focus:outline-hidden focus:text-primary"
-                    href="{{ $item['url'] ?? '#' }}">
+                    href="{{ $item['url'] ?? '#' }}"
+                    aria-label="{{ $item['label'] }}">
                     {{ $item['label'] }}
                     <x-icons.slash class="shrink-0 size-5 text-gray-400 mx-2" />
                 </a>
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,5 +1,5 @@
 <div class="col-span-full">
-    <label for="{{ $name }}" class="sr-only">{{ $label }}</label>
+    <label for="{{ $name }}" class="">{{ $label }}</label>
     <textarea id="{{ $name }}" name="{{ $name }}" rows="4" {{ $attributes }}
         class="py-3 px-4 block w-full border-slate-200 border rounded-lg text-sm focus:border-primary focus:ring-primary disabled:opacity-50 disabled:pointer-events-none"
         placeholder="{{ $label }}"></textarea>
