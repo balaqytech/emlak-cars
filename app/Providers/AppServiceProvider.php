80321c32c24f1350ed6719c5ccd05d090c26c0b0
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-10 05:32:51 +0300

    feat: install and configure seo package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
+use Panakour\FilamentFlatPage\Facades\FilamentFlatPage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -36,5 +38,14 @@ class AppServiceProvider extends ServiceProvider
 
         Paginator::defaultView('components.pagination');
         Paginator::defaultSimpleView('components.pagination');
+
+        $description = str(FilamentFlatPage::get('about.json', 'about_description'))->stripTags()->limit(155)->toString();
+
+        Config::set('seo.site_name', general_settings('site_name'));
+        Config::set('seo.favicon', public_path('storage/' . general_settings('site_favicon')));
+        Config::set('seo.title.suffix', ' - ' . general_settings('site_name'));
+        Config::set('seo.title.homepage_title', general_settings('site_name') . ' - ' . general_settings('site_tagline'));
+        Config::set('seo.description.fallback', $description);
+        Config::set('seo.image.fallback', public_path('storage/' . general_settings('site_banner')));
     }
 }
