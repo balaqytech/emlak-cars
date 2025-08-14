b6c90f9b32ecae4cca0034e27d32fc007900de5c
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-14 22:55:38 +0300

    feat: implement dynamic SEO data generation for offers index page

--- resources/views/pages/offers/index.blade.php
+++ resources/views/pages/offers/index.blade.php
@@ -1,13 +1,73 @@
 @php
+    use RalphJSmit\Laravel\SEO\Support\SEOData;
+    use RalphJSmit\Laravel\SEO\SchemaCollection;
+    use RalphJSmit\Laravel\SEO\Support\AlternateTag;
     use function Laravel\Folio\name;
 
     name('offers.index');
 
-    $offers = \App\Models\Offer::paginate(9);
+    $offers = \App\Models\Offer::latest()->paginate(9);
+
+    $SEO = new SEOData(
+        title: __('frontend.offers.page_title'),
+        description: __('frontend.offers.subheading'),
+        image: asset('storage/' . general_settings('site_banner')),
+        url: localizedUrl('offers'),
+        robots: 'index, follow',
+        alternates: collect(config('app.locales'))
+            ->map(function ($locale) {
+                return new AlternateTag($locale, localizedUrl('offers', $locale));
+            })
+            ->all(),
+        schema: SchemaCollection::make()
+            ->add(
+                fn() => [
+                    '@context' => 'https://schema.org',
+                    '@type' => 'WebPage',
+                    'name' => __('frontend.offers.page_title'),
+                    'description' => __('frontend.offers.subheading'),
+                    'image' => asset('storage/' . general_settings('site_banner')),
+                    'url' => localizedUrl('offers'),
+                    'isPartOf' => [
+                        '@type' => 'WebSite',
+                        'name' => general_settings('site_name'),
+                        'url' => localizedUrl('/'),
+                    ],
+                ],
+            )
+            ->add(function () use ($offers) {
+                if ($offers->isEmpty()) {
+                    return null;
+                }
+
+                return [
+                    '@context' => 'https://schema.org',
+                    '@type' => 'ItemList',
+                    'name' => __('frontend.offers.page_title'),
+                    'itemListOrder' => 'https://schema.org/ItemListOrderAscending',
+                    'numberOfItems' => $offers->count(),
+                    'itemListElement' => $offers
+                        ->map(function ($offer, $index) {
+                            return [
+                                '@type' => 'ListItem',
+                                'position' => $index + 1,
+                                'name' => $offer->title,
+                                'url' => localizedUrl('offers/' . $offer->slug),
+                                'image' => asset('storage/' . $offer->image),
+                            ];
+                        })
+                        ->all(),
+                ];
+            }),
+    );
 @endphp
 
 <x-page-layout>
     <x-slot name="title">
+        {!! seo($SEO) !!}
+    </x-slot>
+
+    <x-slot name="pageTitle">
         {{ __('frontend.offers.page_title') }}
     </x-slot>
 
