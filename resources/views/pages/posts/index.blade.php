ca5a7eb171c8a204623eaf67be99c1f8548dbac8
As3ad Altabali <as3ad.moh@gmail.com>
2025-08-14 23:05:41 +0300

    feat: implement dynamic SEO data generation for posts index page

--- resources/views/pages/posts/index.blade.php
+++ resources/views/pages/posts/index.blade.php
@@ -1,9 +1,70 @@
 @php
+    use RalphJSmit\Laravel\SEO\Support\SEOData;
+    use RalphJSmit\Laravel\SEO\SchemaCollection;
+    use RalphJSmit\Laravel\SEO\Support\AlternateTag;
+
     $posts = \App\Models\Post::orderBy('is_featured', 'desc')->orderBy('published_at', 'desc')->paginate(9);
+
+    $SEO = new SEOData(
+        title: __('frontend.posts.page_title'),
+        description: __('frontend.posts.subheading'),
+        image: asset('storage/' . general_settings('site_banner')),
+        url: localizedUrl('posts'),
+        robots: 'index, follow',
+        alternates: collect(config('app.locales'))
+            ->map(function ($locale) {
+                return new AlternateTag($locale, localizedUrl('posts', $locale));
+            })
+            ->all(),
+        schema: SchemaCollection::make()
+            ->add(
+                fn() => [
+                    '@context' => 'https://schema.org',
+                    '@type' => 'WebPage',
+                    'name' => __('frontend.posts.page_title'),
+                    'description' => __('frontend.posts.subheading'),
+                    'image' => asset('storage/' . general_settings('site_banner')),
+                    'url' => localizedUrl('posts'),
+                    'isPartOf' => [
+                        '@type' => 'WebSite',
+                        'name' => general_settings('site_name'),
+                        'url' => localizedUrl('/'),
+                    ],
+                ],
+            )
+            ->add(function () use ($posts) {
+                if ($posts->isEmpty()) {
+                    return null;
+                }
+
+                return [
+                    '@context' => 'https://schema.org',
+                    '@type' => 'ItemList',
+                    'name' => __('frontend.posts.page_title'),
+                    'itemListOrder' => 'https://schema.org/ItemListOrderAscending',
+                    'numberOfItems' => $posts->count(),
+                    'itemListElement' => $posts
+                        ->map(function ($post, $index) {
+                            return [
+                                '@type' => 'ListItem',
+                                'position' => $index + 1,
+                                'name' => $post->title,
+                                'url' => localizedUrl('posts/' . $post->slug),
+                                'image' => asset('storage/' . $post->image),
+                            ];
+                        })
+                        ->all(),
+                ];
+            }),
+    );
 @endphp
 
 <x-page-layout>
     <x-slot name="title">
+        {!! seo($SEO) !!}
+    </x-slot>
+
+    <x-slot name="pageTitle">
         {{ __('frontend.posts.page_title') }}
     </x-slot>
 
